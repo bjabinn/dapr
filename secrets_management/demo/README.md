d5866ac1498b0c50f95628a7d7ce9db3913d1cac
INSAGS\0017658 <jabeltran@viewnext.com>
2023-07-06 10:15:16 +0200

    secret-management from azure

--- secrets_management/demo/README.md
+++ secrets_management/demo/README.md
@@ -1,2 +1,11 @@
-dapr run --app-id secrets-management --resources-path .\components -- java -jar target\dapr-secret-management-0.0.1-SNAPSHOT.jar
+mvn clean install -DskipTests && dapr run --app-id secrets-management --resources-path .\components -- java -jar target\dapr-secret-management-0.0.1-SNAPSHOT.jar
 
+
+Bibliografia:
+https://github.com/dapr/quickstarts/tree/master/secrets_management/java/sdk
+https://docs.dapr.io/developing-applications/building-blocks/secrets/howto-secrets/
+https://github.com/casperlehmann/akv
+
+
+Mi documentación: 
+https://prezi.com/dashboard/next/#/all
--- secrets_management/demo/src/main/java/com/viewnext/dapr/controllers/secretController.java
+++ secrets_management/demo/src/main/java/com/viewnext/dapr/controllers/secretController.java
@@ -21,12 +21,14 @@ import java.util.Map;
 public class secretController {
     private static final Logger log = LoggerFactory.getLogger(secretController.class);
     private static final ObjectMapper JSON_SERIALIZER = new ObjectMapper();
-    private static final String SECRET_STORE_NAME = "localsecretstore";
+    private static final String SECRET_LOCAL_STORE_NAME = "localsecretstore";
+    private static final String SECRET_AZURE_KEYVALUE_STORE = "azurekeyvault";
 
     @GetMapping("/local")
     public ResponseEntity<String> getLocal() throws JsonProcessingException {
+        log.info("-----------------------------------_-----------------------------DEVOLVIENDO SECRETO DESDE LOCAL.");
         DaprClient client = new DaprClientBuilder().build();
-        Map<String, String> secret = client.getSecret(SECRET_STORE_NAME, "secret").block();
+        Map<String, String> secret = client.getSecret(SECRET_LOCAL_STORE_NAME, "secret").block();
         String result = "Result: " + JSON_SERIALIZER.writeValueAsString(secret);
         ResponseEntity response = new ResponseEntity<>(result, HttpStatus.OK);
         return response;
@@ -34,8 +36,9 @@ public class secretController {
 
     @GetMapping("/azure")
     public ResponseEntity<String> getAzure() throws JsonProcessingException {
+        log.info("-----------------------------------_-----------------------------DEVOLVIENDO SECRETO DESDE AZURE.");
         DaprClient client = new DaprClientBuilder().build();
-        Map<String, String> secret = client.getSecret(SECRET_STORE_NAME, "secret").block();
+        Map<String, String> secret = client.getSecret(SECRET_AZURE_KEYVALUE_STORE, "key1").block();
         String result = "Result: " + JSON_SERIALIZER.writeValueAsString(secret);
         ResponseEntity response = new ResponseEntity<>(result, HttpStatus.OK);
         return response;
