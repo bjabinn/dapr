44d24f57d8d5bece84cb96f9a293f8fd15b38cbb
INSAGS\0017658 <jabeltran@viewnext.com>
2023-07-07 10:22:10 +0200

    refactor folder name pub_sub

---
+++ 4pub_sub/enviaPedidos/src/main/java/com/viewnext/dapr/models/Order.java
@@ -0,0 +1,12 @@
+package com.viewnext.dapr.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Order {
+    private int orderId;
+}
